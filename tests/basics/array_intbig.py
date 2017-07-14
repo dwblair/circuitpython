27d19de2bdf8d2e1b85af4765c567c03fbe140fa
Scott Shawcroft <scott@chickadee.tech>
2017-07-14 15:22:30 -0700

    tests: Skip some tests if long int support isn't available.

--- tests/basics/array_intbig.py
+++ tests/basics/array_intbig.py
@@ -9,6 +9,17 @@ except ImportError:
 print(array('L', [0, 2**32-1]))
 print(array('l', [-2**31, 0, 2**31-1]))
 
+# Skip if long ints are not supported.
+try:
+    # We have to use variables because 1 << 40 causes an exception on parse and
+    # cannot be caught.
+    x = 40
+    x = 1 << x
+except OverflowError:
+    print("SKIP")
+    import sys
+    sys.exit()
+
 print(array('q'))
 print(array('Q'))
 
